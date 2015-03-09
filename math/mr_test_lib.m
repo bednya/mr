9a52877e8c3d681745620c0dbc7ae5233ad73f2a
Alexander Bednyakov <bednya@theor.jinr.ru>
2015-03-09 19:21:50 +0300

    [MATH] new (few pars) functions to get MS pars

--- math/mr_test_lib.m
+++ math/mr_test_lib.m
@@ -1,6 +1,8 @@
 <<"mr_lib.m";
 
+$CharacterEncoding = "UTF8";
 
+SetOptions[$Output, PageWidth->200]
 (*
 
 sol = sol[ PDG`MT][]
@@ -10,17 +12,30 @@ chiSquare[sol]
 
 as = 0.108002; (* gs^2/(4 Pi) /. sol *)
 
-pars = RunParsFromPoleMassesAndAsWithMb[][ as, PDG`MT] 
+SetPDGValues[$DegrassiEtAlValues]
+ReportPDGValues[]
 
+XtQCD[PDG`MB,PDG`MW,PDG`MZ,PDG`MH,PDG`MT,PDG`MT]
+
+
+(*
+pars0 = sol[PDG`MT][]
+parsp10 = RunParsFromPoleMassesAndAsWithMb[][ as, PDG`MT*10] 
+parsm10 = RunParsFromPoleMassesAndAsWithMb[][ as, PDG`MT/10] 
+
+pars1 = RunParsFromPoleMassesAndAsAndGf[][ as, PDG`MT] 
+
+Quit[];
 RunSM[pars, PDG`MZ]
 
 checkMZ = ScaleDependence[MZ][pars]
 
 PDG`MZ
 
-ReportPDGValues[]
+*)
 
 EstimateTheorUncertaintyInMatchingDegrassi[PDG`MT,10]
+
 (* 
 pars /. h->1 /. x_String->1 /. corr[x_,y_]:> y*corr[y-x]
 *)
